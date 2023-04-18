ff2d9a788a0ef1208f1926a68a1e76da39c4a017
Sofia Marchetti <113708530+sofiamarchetti@users.noreply.github.com>
2023-04-17 21:33:17 -0300

    Add files via upload

---
+++ Exercicio3.java
@@ -0,0 +1,33 @@
+package LacosDeRepeticao;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int idade,pesmen21=0,pesmai50=0;
+		
+		System.out.println("\nDigite uma idade: ");
+		idade = leia.nextInt();
+		
+		while(idade > 0) {
+
+	        if(idade <= 21) {
+	        	pesmen21++;
+	        }else {
+	        	pesmai50++;
+	        }
+	        System.out.println("\nDigite uma idade: ");
+	        idade = leia.nextInt();
+
+	    }
+		
+		System.out.println("\nTotal de pessoas menores de 21 anos: "+pesmen21);
+		System.out.println("\nTotal de pessoas maiores de 50 anos: "+pesmai50);
+
+	}
+
+}

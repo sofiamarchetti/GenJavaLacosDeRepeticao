1785bb5a8d53b327d25dc334a9b5083ab710849d
Sofia Marchetti <113708530+sofiamarchetti@users.noreply.github.com>
2023-10-17 11:17:14 -0300

    Resolução Exercício 5

--- Exercicio5.java
+++ Exercicio5.java
@@ -4,23 +4,21 @@ import java.util.Scanner;
 
 public class Exercicio5 {
 
-	public static void main(String[] args) {
-			
-	int numero,soma=0;
-	boolean resultado;
-			
-	Scanner leia = new Scanner(System.in);
-			
-	System.out.println("\nDigite um número: ");
-	numero = leia.nextInt();
-			
-	do {		
-		soma += numero;
-		System.out.println("\nDigite um número: ");
-		
-			} while(numero != 0);
-	
-		System.out.println("\nA soma dos números positivos é: "+soma);
-	}
-	
+    public static void main(String[] args) {
+
+        Scanner leia = new Scanner(System.in);
+
+        int numero, soma = 0;
+
+        do {
+            System.out.println("\nDigite um número (ou 0 para encerrar): ");
+            numero = leia.nextInt();
+
+            if (numero > 0) {
+                soma += numero;
+            }
+        } while (numero != 0);
+
+        System.out.println("\nA soma dos números positivos é: " + soma);
+    }
 }
